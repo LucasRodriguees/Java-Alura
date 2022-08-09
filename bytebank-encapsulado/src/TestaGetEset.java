980451176f206c8c6da00f6d96b4c03909d2085a
lucas oliveira <lucas.orodrigues@hotmail.com>
2022-08-09 18:30:36 -0300

    Java_Alura

---
+++ bytebank-encapsulado/src/TestaGetEset.java
@@ -0,0 +1,10 @@
+
+public class TestaGetEset {
+
+	public static void main(String[] args) {
+		Conta conta = new Conta(1337,24226);
+		conta.setNumero(1337);
+		System.out.println(conta.getNumero());
+	}
+
+}

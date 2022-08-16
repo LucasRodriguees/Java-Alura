48fe96461580ab6ba0769c9fb8ca8da8c49d497e
lucas oliveira <lucas.orodrigues@hotmail.com>
2022-08-16 19:37:20 -0300

    Java_Alura

--- bytebank-herdado/src/Gerente.java
+++ bytebank-herdado/src/Gerente.java
@@ -1,7 +1,11 @@
 
 public class Gerente extends Funcionario implements Autenticavel{
 
-	private int senha;
+	private AutenticacaoUtil autenticador;
+	
+	public Gerente() {
+		this.autenticador = new AutenticacaoUtil();
+	}
 	
 	public double getBonificacao() {
 		System.out.println("chamando o método de bonificacao do Gerente");
@@ -10,16 +14,13 @@ public class Gerente extends Funcionario implements Autenticavel{
 
 	@Override
 	public void setSenha(int senha) {
-	this.senha = senha;
+		this.autenticador.setSenha(senha);
 		
 	}
 
 	@Override
 	public boolean autentica(int senha) {
-		if (this.senha == senha) {
-			return true;
-		}
-		return false;
+		return this.autenticador.autentica(senha);
 	}
 	
 }

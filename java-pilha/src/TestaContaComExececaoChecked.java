f928c83b92bc6665104a01e2f1f07271cd95b6f8
lucas oliveira <lucas.orodrigues@hotmail.com>
2022-08-28 14:51:19 -0300

    java_Alura

---
+++ java-pilha/src/TestaContaComExececaoChecked.java
@@ -0,0 +1,13 @@
+
+public class TestaContaComExececaoChecked {
+
+	public static void main (String [] args) {
+		
+		Conta conta = new Conta();
+		try {
+		conta.deposita();
+		}catch(MinhaExcecao ex) {
+			System.out.println("tratamento ...");
+		}
+	}
+}

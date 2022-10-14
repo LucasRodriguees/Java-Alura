47c7eb2e9070befd8d7a793a43aa22e5adb572d2
lucas oliveira <lucas.orodrigues@hotmail.com>
2022-10-13 21:26:00 -0300

    Alura_Streams

--- Java8/src/teste.java
+++ Java8/src/teste.java
@@ -1,6 +1,10 @@
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
+import java.util.OptionalDouble;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class teste {
@@ -14,17 +18,31 @@ public class teste {
 		cursos.add(new Curso("c", 55)); 
 		
 		cursos.sort(Comparator.comparingInt(c -> c.getAlunos()));
-
-		Stream<Curso> streamDeCurso = cursos.stream()
-				.filter(c -> c.getAlunos() > 50);
-		streamDeCurso.forEach(c -> System.out.println(c.getNome()));
+			
+		OptionalDouble media = cursos.stream()
+		.filter(c -> c.getAlunos() >= 100)
+		.mapToInt(c -> c.getAlunos())
+		.average();
 		
-		Stream<String> nomes = cursos.stream().map(Curso::getNome);
+//		System.out.println(media);
+		
+		
+				cursos	= cursos.stream()
+				.filter(c -> c.getAlunos() >= 100)
+				.collect(Collectors.toList());
 		
 		cursos.stream()
-		   .filter(c -> c.getAlunos() > 50)
+		.forEach(c -> System.out.println(c.getNome()));
+				
+				
+		Stream<String> nomes = cursos.stream().map(Curso::getNome);	
+		
+		cursos.stream()
+		   .filter(c -> c.getAlunos() >= 40)
 		   .map(Curso::getAlunos)
 		   .forEach(System.out::println);
+		
+		
 
 
 	}
